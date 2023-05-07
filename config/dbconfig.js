d154ad97c7cbb50aac127d46fd1a2d42806b5945
Auditya Maulana Adnan <91017662+aud1tya4dnan0@users.noreply.github.com>
2023-05-07 09:25:48 +0700

    upload

---
+++ config/dbconfig.js
@@ -0,0 +1,13 @@
+
+
+const connStr = {
+    MSSQL: {
+        USER: 'integratif',
+        PASSWORD: 'G3rb4ng!',
+        SERVER: '10.199.14.47',
+        DATABASE: 'GATE_DEV',
+    }
+};
+
+
+module.exports = connStr;
